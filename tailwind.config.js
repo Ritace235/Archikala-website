c0b5f7374822b2ff619947c5b661d7d09d9ab30d
Ritace235 <ar.staritesh3653@gmail.com>
2025-06-19 22:42:11 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        coral: '#ff7f50',
+      },
+    },
+  },
+  plugins: [],
+}; 
